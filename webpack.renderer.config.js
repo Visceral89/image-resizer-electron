3513eee07d7ececf37de5b805637ca3cd573943a
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-08-14 08:35:23 +0200

    SCSS working

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,13 +1,22 @@
-const rules = require('./webpack.rules');
+const rules = require("./webpack.rules");
+const path = require("path");
 
 rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+	test: /\.scss$/,
+	use: [
+		{ loader: "style-loader" },
+		{ loader: "css-loader" },
+		{ loader: "sass-loader" },
+	],
 });
 
 module.exports = {
-  // Put your normal webpack config below here
-  module: {
-    rules,
-  },
+	module: {
+		rules,
+	},
+	// Make sure to specify where the output should go
+	output: {
+		filename: "renderer.js",
+		path: path.resolve(__dirname, "dist"),
+	},
 };
