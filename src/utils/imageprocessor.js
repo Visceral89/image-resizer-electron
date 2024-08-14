3d35b69085ca0d68e2adb4ca042512a9bc1ecf21
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-08-14 10:54:26 +0200

    Outline the functions in imageprocessor

---
+++ src/utils/imageprocessor.js
@@ -0,0 +1,17 @@
+const { sizes } = require("../constants/constants.js");
+
+function drop(event) {}
+
+function open_folder() {}
+
+function resize_image() {
+	return;
+}
+
+function resize_gif() {
+	return;
+}
+
+function save_images(image, output_path) {}
+
+function process_images() {}
