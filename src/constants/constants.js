9ac18a76c287afcead2eaf8cc9799c5d81275454
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-08-15 08:52:06 +0200

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,6 @@
 const WINDOW_TITLE = "Image Resizer";
 const WINDOW_HEIGHT = 600;
 const WINDOW_WIDTH = 800;
+const SIZES = [512, 112, 56, 32];
 
-module.exports = { WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE };
+module.exports = { WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE, SIZES };
